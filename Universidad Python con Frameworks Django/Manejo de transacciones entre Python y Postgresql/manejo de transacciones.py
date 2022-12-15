61bbedcbd883c912ad8105aa2836258738deb4eb
Alejandro Steinman <steinman920@gmail.com>
2022-12-15 17:20:40 -0500

    transaccion

--- Universidad Python con Frameworks Django/Manejo de transacciones entre Python y Postgresql/manejo de transacciones.py
+++ Universidad Python con Frameworks Django/Manejo de transacciones entre Python y Postgresql/manejo de transacciones.py
@@ -6,10 +6,13 @@ try:
     sentencia = "INSERT INTO persona(nombre,apellido,email) VALUES(%s,%s,%s)"
     valores = ("Maria","Esparta","Mesperta@gmail.com")
     cursor.execute(sentencia,valores)
-    print("Transaccion finalizada")
+    sentencia = "UPDATE persona SET nombre=%s, apellido=%s,email=%s WHERE id_persona=%s"
+    valores = ("Juan carlos","Juarez","jcjuares@gmail.com",1)
+    cursor.execute(sentencia,valores)
     conexion.commit()
+    print("Transaccion finalizada")
 except Exception as e:
     conexion.rollback()
-    print(f"Ocurrio un error, se hizo rollback{e}")
+    print(f"Ocurrio un error, se hizo rollback: {e}")
 finally:
     conexion.close()
