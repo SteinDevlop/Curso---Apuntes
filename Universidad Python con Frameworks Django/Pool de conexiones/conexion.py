908fe8ce7f954a679428d5f9b910d318481d62ad
Alejandro Steinman <steinman920@gmail.com>
2022-12-27 19:25:57 -0500

    Pool de Conexiones con Python y Postgresql

---
+++ Universidad Python con Frameworks Django/Pool de conexiones/conexion.py
@@ -0,0 +1,41 @@
+import psycopg2 as db
+from psycopg2 import pool
+import sys
+from manejo_de_loggin import *
+
+class Conexion:
+    _DATABASE = "pyUni_db"
+    _USERNAME = 'postgres'
+    _PASSWORD = "admin"
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _MIN_CONEXION = 1
+    _MAX_CONEXION = 5
+    _POOL = None
+
+    @classmethod
+    def obtener_pool(cls):
+        if cls._POOL is None:
+            try:
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CONEXION,cls._MAX_CONEXION,
+                host = cls._HOST,
+                user = cls._USERNAME,
+                password = cls._PASSWORD,
+                port = cls._DB_PORT,
+                database = cls._DATABASE
+                )  # la forma mas facil de obtener un pool
+                log.debug(f"Creacion del pool exitosa:{cls._pool}")
+                return cls._pool
+            except Exception as e:
+                log.error(f"Ha ocurrido un error al inicializar el pool. Error:{e}")
+        else:
+            return cls._pool
+    @classmethod
+    def obtenerconexion(cls):
+        pass
+    @classmethod
+    def obtener_pool(cls):
+        pass
+if __name__ == "__main__":
+    Conexion.obtenerconexion()
+    Conexion.obtenercursor()
